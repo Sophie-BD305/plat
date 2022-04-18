cbd9644d75d35829a07ea99fd7be7d2b2d431878
Sophie-BD305 <mogoo@kakao.com>
2022-04-19 04:09:38 +0900

    w3w complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:what3words/what3words.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,34 +10,16 @@ void main() async {
   runApp(const MyApp());
 }
 
-var api = What3WordsV3('what3words-api-key');
+var api = What3WordsV3('MABA5QIB');
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  void wtw() async {
-    var words = await api
-        .convertTo3wa(Coordinates(51.508344, -0.12549900))
-        .language('en')
-        .execute();
-    return words.data()?.toJson();
-  }
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -46,16 +29,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -68,32 +41,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  void searchlocation() async {
+    var words = await api
+        .convertTo3wa(Coordinates(51.508344, -0.12549900))
+        .language('en')
+        .execute();
+    print('Words: ${words.data()?.toJson()}');
+  }
+
+  void findlocation() async {
+    var coordinates =
+        await api.convertToCoordinates('index.home.raft').execute();
+
+    if (coordinates.isSuccessful()) {
+      print('Coordinates ${coordinates.data()!.toJson()}');
+    } else {
+      var error = coordinates.error();
+
+      if (error == What3WordsError.BAD_WORDS) {
+        // The three word address provided is invalid
+        print('BadWords: ${error?.message}');
+      } else if (error == What3WordsError.INTERNAL_SERVER_ERROR) {
+        // Server Error
+        print('InternalServerError: ${error?.message}');
+      } else if (error == What3WordsError.NETWORK_ERROR) {
+        // Network Error
+        print('NetworkError: ${error?.message}');
+      } else {
+        print('${error?.code} : ${error?.message}');
+      }
+    }
+  }
+
+  void fetchPost() {
+    print(http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1')));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -106,8 +100,14 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text('제발 돼라!!!!!'),
             ),
-            const Text(
-              'You have pushed the button this many times:',
+            TextButton(
+              onPressed: () {
+                searchlocation();
+              },
+              child: const Text('이건?'),
+            ),
+            Text(
+              '$fetchPost()',
             ),
             Text(
               '$_counter',
@@ -120,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
